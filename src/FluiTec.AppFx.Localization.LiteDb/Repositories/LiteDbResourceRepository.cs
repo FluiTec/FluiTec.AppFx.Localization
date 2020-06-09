cb296fbdcb2f0c8ca479653bff20e187d590b178
Achim Schnell <a.schnell@wtschnell.de>
2020-06-09 13:00:08 +0200

    ...

--- src/FluiTec.AppFx.Localization.LiteDb/Repositories/LiteDbResourceRepository.cs
+++ src/FluiTec.AppFx.Localization.LiteDb/Repositories/LiteDbResourceRepository.cs
@@ -2,18 +2,34 @@
 using FluiTec.AppFx.Data.LiteDb.UnitsOfWork;
 using FluiTec.AppFx.Data.Repositories;
 using FluiTec.AppFx.Localization.Entities;
+using FluiTec.AppFx.Localization.Repositories;
 using Microsoft.Extensions.Logging;
 
 namespace FluiTec.AppFx.Localization.LiteDb.Repositories
 {
     /// <summary>   A lite database resource repository. </summary>
-    public class LiteDbResourceRepository : LiteDbWritableIntegerKeyTableDataRepository<ResourceEntity>
+    public class LiteDbResourceRepository : LiteDbWritableIntegerKeyTableDataRepository<ResourceEntity>, IResourceRepository
     {
         /// <summary>   Constructor. </summary>
         /// <param name="unitOfWork">   The unit of work. </param>
         /// <param name="logger">       The logger. </param>
         public LiteDbResourceRepository(LiteDbUnitOfWork unitOfWork, ILogger<IRepository> logger) : base(unitOfWork, logger)
         {
+        }
+
+        public ResourceEntity GetByKey(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ResetSyncStatus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RefactorKey(string oldKey, string newKey)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- src/FluiTec.AppFx.Localization.LiteDb/Repositories/LiteDbTranslationRepository.cs
+++ src/FluiTec.AppFx.Localization.LiteDb/Repositories/LiteDbTranslationRepository.cs
@@ -1,19 +1,32 @@
-﻿using FluiTec.AppFx.Data.LiteDb.Repositories;
+﻿using System.Collections.Generic;
+using FluiTec.AppFx.Data.LiteDb.Repositories;
 using FluiTec.AppFx.Data.LiteDb.UnitsOfWork;
 using FluiTec.AppFx.Data.Repositories;
+using FluiTec.AppFx.Localization.Compound;
 using FluiTec.AppFx.Localization.Entities;
+using FluiTec.AppFx.Localization.Repositories;
 using Microsoft.Extensions.Logging;
 
 namespace FluiTec.AppFx.Localization.LiteDb.Repositories
 {
     /// <summary>   A lite database translation repository. </summary>
-    public class LiteDbTranslationRepository : LiteDbWritableIntegerKeyTableDataRepository<TranslationEntity>
+    public class LiteDbTranslationRepository : LiteDbWritableIntegerKeyTableDataRepository<TranslationEntity>, ITranslationRepository
     {
         /// <summary>   Constructor. </summary>
         /// <param name="unitOfWork">   The unit of work. </param>
         /// <param name="logger">       The logger. </param>
         public LiteDbTranslationRepository(LiteDbUnitOfWork unitOfWork, ILogger<IRepository> logger) : base(unitOfWork, logger)
         {
+        }
+
+        public IEnumerable<TranslationEntity> ByResource(ResourceEntity resource)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<CompoundTranslationEntity> GetAllCompound()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
