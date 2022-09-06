e5caa389e9349b614b90f92097d0b8ec1132395f
Achim Schnell <a.schnell@wtschnell.de>
2022-09-06 10:18:22 +0200

    add another extension method

--- src/FluiTec.AppFx.Localization/Extensions/StringLocalizerExtension.cs
+++ src/FluiTec.AppFx.Localization/Extensions/StringLocalizerExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq.Expressions;
 using System.Reflection;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Localization;
 
 namespace FluiTec.AppFx.Localization.Extensions
@@ -26,6 +27,27 @@ namespace FluiTec.AppFx.Localization.Extensions
         /// A string.
         /// </returns>
         public static string Localize<TResource>(this IStringLocalizer<TResource> localizer,
+            Expression<Func<TResource, object>> localized, params object[] args)
+        {
+            return Localize(localizer as IStringLocalizer, localized, args);
+        }
+
+        /// <summary>
+        /// An IStringLocalizer&lt;TResource&gt; extension method that localizes.
+        /// </summary>
+        ///
+        /// <exception cref="ArgumentException">    Thrown when one or more arguments have unsupported or
+        ///                                         illegal values. </exception>
+        ///
+        /// <typeparam name="TResource">    Type of the resource. </typeparam>
+        /// <param name="localizer">    The localizer to act on. </param>
+        /// <param name="localized">    The localized. </param>
+        /// <param name="args">         A variable-length parameters list containing arguments. </param>
+        ///
+        /// <returns>
+        /// A string.
+        /// </returns>
+        public static string Localize<TResource>(this IStringLocalizer localizer,
             Expression<Func<TResource, object>> localized, params object[] args)
         {
             var member = localized.Body as MemberExpression;
